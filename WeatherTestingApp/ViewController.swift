c149c3f9df3d89c0a88091c7426b53475fb8f6b8
buritos77 <buritos77@gmail.com>
2020-05-20 14:51:18 +0300

    name - api

--- WeatherTestingApp/ViewController.swift
+++ WeatherTestingApp/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var weatherDescriptionLabel: UILabel!
     @IBOutlet weak var weatherImageView: UIImageView!
-    //TODO: i need make to work city label
+    //TODO: i need to make city label
     @IBOutlet weak var weatherCityLabel: UILabel!
     
     
@@ -30,11 +30,14 @@ class ViewController: UIViewController {
             if let data = data, error == nil {
             do {
                 guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : Any] else { return }
-                guard let weatherDetails = json["weather"] as? [[String : Any]], let weatherMain = json["main"] as? [String : Any] else {return}
+                guard let weatherDetails = json["weather"] as? [[String : Any]], let weatherMain = json["main"] as? [String : Any],
+                    //MARK: ТУТ ТРАБЛЫ!
+                    let weatherDetailsName = json["coord"] as? [String : Any] else {return}
                 let temp = Int(weatherMain["temp"] as? Double ?? 0)
                 let description = (weatherDetails.first?["description"] as? String)?.capitalizingFirstLetter()
+                let name = (weatherDetailsName["name"] as? String)
                 DispatchQueue.main.async {
-                    self.setWeather(weather: weatherDetails.first?["main"] as? String, description: description, temp: temp)
+                    self.setWeather(weather: weatherDetails.first?["main"] as? String, description: description, temp: temp, name: name)
                 }
             }
             catch{
@@ -46,10 +49,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    func setWeather(weather: String?, description: String?, temp: Int) {
-        // here weather conditions icons https://openeathermaporg/weather-conditions
+    func setWeather(weather: String?, description: String?, temp: Int, name: String?) {
+        // here weather conditions icons help https://openeathermaporg/weather-conditions
         weatherDescriptionLabel.text = description ?? "..."
         tempLabel.text = "\(temp-273)º"
+        weatherCityLabel.text = name ?? "..."
         switch weather {
         case "Sunny":
             weatherImageView.image = UIImage(named: "sunny")
