56f3546c759742d34a0dd7d77b85f2233886b9c6
buritos77 <buritos77@gmail.com>
2020-05-21 13:36:48 +0300

    ver 0.2

--- WeatherTestingApp/ViewController.swift
+++ WeatherTestingApp/ViewController.swift
@@ -18,7 +18,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var weatherCityLabel: UILabel!
     
     
+    struct WeatherUrl : Decodable{
+        //let main :String?
+        var name : String?
+        var main : WeatherMain?
+        var weather : weatherDesc?
+    }
     
+    struct WeatherDesc 
+    
+    struct WeatherMain : Decodable{
+        var temp: Double
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +44,15 @@ class ViewController: UIViewController {
                     let weatherDetailsName = json as? [String : Any] else {return}
                 let temp = Int(weatherMain["temp"] as? Double ?? 0)
                 let description = (weatherDetails.first?["description"] as? String)?.capitalizingFirstLetter()
+                
+                
+                let weatherUrl = try JSONDecoder().decode(WeatherUrl.self, from : data)
+                print(weatherUrl)
+                
                 let name = (weatherDetailsName["name"] as? String)
                 DispatchQueue.main.async {
                     self.setWeather(weather: weatherDetails.first?["main"] as? String, description: description, temp: temp, name: name)
+
                 }
             }
             catch{
@@ -44,28 +61,37 @@ class ViewController: UIViewController {
             }
         }
         task.resume()
-        // Do any additional setup after loading the view.
+
     }
-    
+    //IT NEEDS TO BE HERE
     func setWeather(weather: String?, description: String?, temp: Int, name: String?) {
-        // here weather conditions icons help https://openeathermaporg/weather-conditions
+        // here weather conditions icons help https://openeathermap.org/weather-conditions
         weatherDescriptionLabel.text = description ?? "..."
         tempLabel.text = "\(temp-273)º"
         weatherCityLabel.text = name ?? "..."
         switch weather {
-        case "Sunny":
+        case "Clear":
             weatherImageView.image = UIImage(named: "sunny")
             //backgroung changing night day
-        
+        case "Clouds":
+            weatherImageView.image = UIImage(named: "cloudy")
+        case "Snow":
+            weatherImageView.image = UIImage(named: "cloudy")
+        case "Rain":
+            weatherImageView.image = UIImage(named: "cloudy")
+        case "Drizzle":
+            weatherImageView.image = UIImage(named: "cloudy")
+        case "Thunderstorm":
+            weatherImageView.image = UIImage(named: "cloudy")
         default:
-        weatherImageView.image = UIImage(named: "cloudy")
+            weatherImageView.image = UIImage(named: "cloudy")
         }
     
 
     }
 
 }
-
+//MOVE TO ANOTHER FILE
 extension String {
     func capitalizingFirstLetter() -> String {
         return prefix(1).uppercased() + self.lowercased().dropFirst()
